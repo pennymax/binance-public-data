e9083fd7bfdd13b107aaa36940f519933ce99abd
pennymax <pennymaxlw@gmail.com>
2023-07-11 03:44:00 +0000

    support downlaod fundingrate private

--- python/download-kline.py
+++ python/download-kline.py
@@ -113,4 +113,3 @@ if __name__ == "__main__":
         download_monthly_klines(args.type, symbols, num_symbols, args.intervals, args.years, args.months, args.startDate, args.endDate, args.folder, args.checksum)
     if args.skip_daily == 0:
       download_daily_klines(args.type, symbols, num_symbols, args.intervals, dates, args.startDate, args.endDate, args.folder, args.checksum)
-
